87a903167ce1fdf30448708f1ee8ad9cc9b0ee6d
Vaishnavi-Devapuja <141746248+Vaishnavi-Devapuja@users.noreply.github.com>
2023-09-20 22:26:44 +0530

    Update app.js

--- todo-app/app.js
+++ todo-app/app.js
@@ -76,24 +76,23 @@ app.put("/todos/:id/markAsCompleted", async (req, res) => {
 });
 
 app.delete("/todos/:id", async (req, res) => {
-  console.log("To delete a TODO with ID: ", request.params.id);
+   console.log("To delete a to-do with ID: ", request.params.id);
   try {
-    const deletedTodoCount = await Todo.destroy({
-      where: { id: req.params.id },
-    });
-    if (deletedTodoCount === 0) {
-      return res.status(422).json({ error: "Todo not found" });
+    const todo = await Todo.findByPk(req.params.id);
+    if (!todo) {
+      return res.status(404).json({ error: 'Todo not found' });
     }
-    res.json({ success: true });
+
+    const deleted = await todo.destroy();
+    if (deleted) {
+      res.json({ success: true });
+    } else {
+      res.json({ success: false });
+    }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Server error" });
+    res.status(422).json({ error: 'Server error' });
   }
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
